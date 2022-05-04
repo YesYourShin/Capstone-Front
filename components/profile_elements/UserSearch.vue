fba68595dd280483864a049ac90e64267b2ac10c
lein-hub <75424893+lein-hub@users.noreply.github.com>
2022-05-04 16:23:49 +0900

    search bar minor fix

--- components/profile_elements/UserSearch.vue
+++ components/profile_elements/UserSearch.vue
@@ -2,17 +2,34 @@
   <div class="w-full p-1 border-r-[3px] border-black">
     <div class="text-sm text-gray-400">Search User</div>
     <form
-      class="h-12 flex items-center justify-center"
+      class="h-12 flex items-center justify-center border border-zinc-700 px-1"
       action=""
       @submit.prevent="search"
     >
       <input
-        class="w-5/6 outline-none p-1 text-white"
+        class="w-5/6 outline-none h-9 p-1 text-white"
         style="background-color: rgb(48, 48, 48)"
         type="text"
         v-model="input"
       />
-      <button class="w-1/6 bg-gray-400 p-1">Search</button>
+      <button
+        class="w-1/6 bg-slate-600 hover:bg-slate-500 transition duration-300 h-9 p-1 flex justify-center items-center"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
     </form>
   </div>
 </template>
